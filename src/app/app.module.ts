ac561ff790356348495dae84c7c3e631a11eca75
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-03-28 11:15:57 +0300

    chore: remove routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
 
@@ -11,8 +10,7 @@ import { TerminalComponent } from './terminal/terminal.component';
     TerminalComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
