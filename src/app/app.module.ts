761c1ccc20382b579a108d48dec9d13b95d14139
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-03-30 18:26:00 +0300

    feat: add projects section with cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
 import { MinecraftComponent } from './minecraft/minecraft.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TerminalComponent,
-    MinecraftComponent
+    MinecraftComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/project/project.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hd-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss']
+})
+export class ProjectComponent implements OnInit {
+  @Input() name = "Project Name";
+  @Input() description = "A simple information about the project";
+  @Input() version = "1.0.0"
+  @Input() state?: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
